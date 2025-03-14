e3bdc0718abcc9337f5ac361c0275fdc6c49ec96
FelixEcca <163112991+FelixEcca@users.noreply.github.com>
2025-03-14 16:27:44 +0100

    Update 303-2024-1D-Gleichungen-und-Funktionen.tsx

--- src/content/implementations/BW_2BFS/303-2024-1D-Gleichungen-und-Funktionen.tsx
+++ src/content/implementations/BW_2BFS/303-2024-1D-Gleichungen-und-Funktionen.tsx
@@ -1,7 +1,20 @@
 import { Exercise } from '@/data/types'
+import { Color2, Color3 } from '@/helper/colors'
 import { buildInlineFrac } from '@/helper/math-builder'
+import { pp } from '@/helper/pretty-print'
 
-interface DATA {}
+interface DATA {
+  bool1: boolean
+  x1: number
+  random: number
+  case: number
+  value: number
+  xs: number
+  ys: number
+  bool2: boolean
+  num: number
+  m: number
+}
 
 export const exercise303: Exercise<DATA> = {
   title: 'Gleichungen und Funktionen',
@@ -10,9 +23,31 @@ export const exercise303: Exercise<DATA> = {
   duration: 42,
   points: 42,
   generator(rng) {
-    return {}
+    return {
+      bool1: rng.randomBoolean(),
+      bool2: rng.randomBoolean(),
+      x1: rng.randomIntBetween(-3, 5),
+      random: rng.randomIntBetween(1, 2),
+      case: rng.randomIntBetween(1, 2),
+      value: rng.randomIntBetween(-5, -2),
+      xs: rng.randomIntBetween(-5, 5),
+      ys: rng.randomIntBetween(-5, 5),
+      num: rng.randomIntBetween(2, 6),
+      m: rng.randomIntBetween(2, 6),
+    }
   },
-  originalData: {},
+  originalData: {
+    bool2: true,
+    bool1: true,
+    x1: 0,
+    random: 1,
+    case: 1,
+    value: -2,
+    xs: -1,
+    ys: -3,
+    num: 5,
+    m: 2,
+  },
   constraint({ data }) {
     return true
   },
@@ -25,26 +60,103 @@ export const exercise303: Exercise<DATA> = {
         </p>
         <ul>
           <li>
-            Die Parabel mit der Gleichung y = x² verläuft durch den Punkt
-            Q(0|0).
-          </li>
-          <li>Jede Parabel schneidet die y-Achse.</li>
-          <li>
-            Das Schaubild von y = -2 ist eine Gerade mit negativer Steigung.
+            Die Parabel mit der Gleichung y = x² verläuft durch den Punkt Q(
+            {pp(data.x1)}|
+            {data.bool1 ? data.x1 * data.x1 : data.x1 * data.x1 + data.random}).
           </li>
           <li>
-            Die Parabel p mit y = (x + 1)² - 3 hat ihren Scheitel bei S(1|-3).
+            {data.case == 1 ? (
+              <>Jede Parabel schneidet die y-Achse.</>
+            ) : (
+              <>Jede Parabel schneidet die x-Achse.</>
+            )}
           </li>
           <li>
-            x = -5 ist eine Lösung der Gleichung{' '}
-            {buildInlineFrac(<>1</>, <>-x-5</>)} +{' '}
-            {buildInlineFrac(<>1</>, <>5</>)}x = 0
+            Das Schaubild von y ={' '}
+            {data.random == 1 ? <>{pp(-data.m)}</> : <>{pp(-data.m)}x</>} ist
+            eine Gerade mit negativer Steigung.
+          </li>
+          <li>
+            Die Parabel p mit y = (x {pp(data.xs, 'merge_op')})²{' '}
+            {pp(data.ys, 'merge_op')} hat ihren Scheitel bei S(
+            {data.bool2 ? <>{pp(data.xs)}</> : <>{pp(-data.xs)}</>}|
+            {pp(data.ys)}).
+          </li>
+          <li>
+            x = {pp(-data.num)} ist eine Lösung der Gleichung{' '}
+            {buildInlineFrac(<>1</>, <>x {pp(-data.num)}</>)} +{' '}
+            {buildInlineFrac(<>1</>, <>{data.num}</>)}x = 0
           </li>
         </ul>
       </>
     )
   },
   solution({ data }) {
-    return <></>
+    return (
+      <>
+        <ul>
+          <li>
+            Aussage 1:{' '}
+            {data.bool1 ? (
+              <>
+                <Color2>Richtig</Color2>
+              </>
+            ) : (
+              <>
+                <Color3>Falsch</Color3>
+              </>
+            )}
+          </li>
+          <li>
+            Aussage 2:{' '}
+            {data.case == 1 ? (
+              <>
+                <Color2>Richtig</Color2>
+              </>
+            ) : (
+              <>
+                <Color3>Falsch</Color3>
+              </>
+            )}
+          </li>
+          <li>
+            Aussage 3:{' '}
+            {data.random != 1 ? (
+              <>
+                <Color2>Richtig</Color2>
+              </>
+            ) : (
+              <>
+                <Color3>Falsch</Color3>
+              </>
+            )}
+          </li>
+          <li>
+            Aussage 4:{' '}
+            {!data.bool2 ? (
+              <>
+                <Color2>Richtig</Color2>
+              </>
+            ) : (
+              <>
+                <Color3>Falsch</Color3>
+              </>
+            )}
+          </li>
+          <li>
+            Aussage 5:{' '}
+            {1 / (-2 * data.num) + 1 != 0 ? (
+              <>
+                <Color3>Falsch</Color3>
+              </>
+            ) : (
+              <>
+                <Color2>Richtig</Color2>
+              </>
+            )}
+          </li>
+        </ul>
+      </>
+    )
   },
 }
