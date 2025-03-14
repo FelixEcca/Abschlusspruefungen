5231a925bca27e1bf27093af47ab2c1aca1fbb93
FelixEcca <163112991+FelixEcca@users.noreply.github.com>
2025-03-14 13:59:52 +0100

    1A fertig

--- src/content/implementations/BW_2BFS/300-2024-1A-Ebene-Figur.tsx
+++ src/content/implementations/BW_2BFS/300-2024-1A-Ebene-Figur.tsx
@@ -1,6 +1,17 @@
 import { Exercise } from '@/data/types'
+import { Color1, Color2, Color3, Color4 } from '@/helper/colors'
+import {
+  buildEquation,
+  buildInlineFrac,
+  buildSqrt,
+} from '@/helper/math-builder'
+import { pp } from '@/helper/pretty-print'
 
-interface DATA {}
+interface DATA {
+  umfang: number
+  x: number
+  länge: number
+}
 
 export const exercise300: Exercise<DATA> = {
   title: 'Ebene Figur',
@@ -8,10 +19,15 @@ export const exercise300: Exercise<DATA> = {
   useCalculator: false,
   duration: 42,
   generator(rng) {
-    return {}
+    return {
+      umfang: rng.randomIntBetween(8, 17) * 2,
+      x: rng.randomIntBetween(2, 7),
+      länge: rng.randomIntBetween(2, 5),
+    }
   },
-  originalData: {},
+  originalData: { umfang: 22, x: 5, länge: 3 },
   constraint({ data }) {
+    data.umfang > 4 * data.x + 2
     return true
   },
   intro({ data }) {
@@ -20,6 +36,12 @@ export const exercise300: Exercise<DATA> = {
         <p>Gegeben ist die symmetrische Figur.</p>
         <svg viewBox="0 0 328 260">
           <image href="/content/BW_2BFS/300.png" height="230" width="328" />
+          <text x={70} y={175} fontSize={20} textAnchor="right" stroke="black">
+            {data.länge} cm
+          </text>
+          <text x={200} y={55} fontSize={20} textAnchor="right" stroke="black">
+            {data.länge} cm
+          </text>
         </svg>
       </>
     )
@@ -34,14 +56,71 @@ export const exercise300: Exercise<DATA> = {
         return (
           <>
             <p>
-              Der Umfang soll 22 cm betragen. Geben Sie dazu eine Gleichung an
-              und berechnen Sie x.
+              Der Umfang soll {data.umfang} cm betragen. Geben Sie dazu eine
+              Gleichung an und berechnen Sie x.
             </p>
           </>
         )
       },
       solution({ data }) {
-        return <></>
+        return (
+          <>
+            <p>Der Umfang ist die Summer aller einzelnen Seitenlängen:</p>
+            {buildEquation([
+              [
+                <>U</>,
+                <>=</>,
+                <>
+                  x + x + x + x + {data.länge} + {data.länge}
+                </>,
+              ],
+              [
+                '',
+                <>
+                  {' '}
+                  <Color4>
+                    <span className="inline-block  scale-y-[1.5]">↓</span>
+                  </Color4>
+                </>,
+                <>
+                  <Color4>
+                    <span style={{ fontSize: 'small' }}>Zusammenfassen</span>
+                  </Color4>
+                </>,
+              ],
+              [<>U</>, <>=</>, <>4x + {2 * data.länge}</>],
+              [
+                '',
+                <>
+                  {' '}
+                  <Color4>
+                    <span className="inline-block  scale-y-[1.5]">↓</span>
+                  </Color4>
+                </>,
+                <>
+                  <Color4>
+                    <span style={{ fontSize: 'small' }}>
+                      Der Umfang soll {data.umfang} cm betragen.
+                    </span>
+                  </Color4>
+                </>,
+              ],
+              [
+                <>{data.umfang}</>,
+                <>=</>,
+                <>4x + {2 * data.länge}</>,
+                <>| − {2 * data.länge}</>,
+              ],
+              [
+                <>{data.umfang - 2 * data.länge}</>,
+                <>=</>,
+                <>4x</>,
+                <>| : 4</>,
+              ],
+              [<>x</>, <>=</>, <>{pp((data.umfang - 2 * data.länge) / 4)}</>],
+            ])}
+          </>
+        )
       },
     },
     {
@@ -53,14 +132,56 @@ export const exercise300: Exercise<DATA> = {
         return (
           <>
             <p>
-              Nun soll x = 5 cm sein. Berechnen Sie die Höhe und geben Sie das
-              Ergebnis als Wurzel an.
+              Nun soll x = {data.x} cm sein. Berechnen Sie die Höhe und geben
+              Sie das Ergebnis als Wurzel an.
             </p>
           </>
         )
       },
       solution({ data }) {
-        return <></>
+        return (
+          <>
+            <p>
+              Im rechtwinkligen Dreieck können wir h mit dem Satz des Pythagoras
+              berechnen:
+            </p>
+            {buildEquation([
+              [<>h²</>, <>=</>, <>x² + x²</>],
+              [
+                '',
+                <>
+                  {' '}
+                  <Color4>
+                    <span className="inline-block  scale-y-[1.5]">↓</span>
+                  </Color4>
+                </>,
+                <>
+                  <Color4>
+                    <span style={{ fontSize: 'small' }}>
+                      Länge für x einsetzen
+                    </span>
+                  </Color4>
+                </>,
+              ],
+              [
+                <>h²</>,
+                <>=</>,
+                <>
+                  {data.x}² + {data.x}²
+                </>,
+              ],
+              [
+                <>h²</>,
+                <>=</>,
+                <>
+                  {data.x * data.x} + {data.x * data.x}
+                </>,
+              ],
+              [<>h²</>, <>=</>, <>{data.x * data.x * 2}</>, <>| √</>],
+              [<>h</>, <>=</>, <>{buildSqrt(data.x * data.x * 2)}</>],
+            ])}
+          </>
+        )
       },
     },
     {
@@ -79,7 +200,100 @@ export const exercise300: Exercise<DATA> = {
         )
       },
       solution({ data }) {
-        return <></>
+        return (
+          <>
+            <p>
+              Die Fläche setzt sich aus einem Rechteck in der Mitte und zwei
+              Dreiecken zusammen.
+            </p>
+            <p>
+              Beachte, dass die Dreiecke zusammengesetzt ein Quadrat mit der
+              Seitenlänge x ergeben.
+            </p>
+            <svg viewBox="0 0 328 260">
+              <image
+                href="/content/BW_2BFS/300_2.png"
+                height="230"
+                width="328"
+              />
+              <text
+                x={70}
+                y={195}
+                fontSize={20}
+                textAnchor="right"
+                stroke="black"
+              >
+                {data.länge} cm
+              </text>
+              <text
+                x={210}
+                y={55}
+                fontSize={20}
+                textAnchor="right"
+                stroke="black"
+              >
+                {data.länge} cm
+              </text>
+            </svg>
+            {buildEquation([
+              [
+                '',
+                <>
+                  {' '}
+                  <Color4>
+                    <span className="inline-block  scale-y-[1.5]">↓</span>
+                  </Color4>
+                </>,
+                <>
+                  <Color4>
+                    <span style={{ fontSize: 'small' }}>
+                      Fläche der zwei grünen Dreiecke und ein oranges Rechteck
+                    </span>
+                  </Color4>
+                </>,
+              ],
+              [
+                <>A</>,
+                <>=</>,
+                <>
+                  <Color2>{buildInlineFrac(<>x · x</>, <>2</>)}</Color2> +{' '}
+                  <Color3>{data.länge} · h</Color3> +{' '}
+                  <Color2>{buildInlineFrac(<>x · x</>, <>2</>)}</Color2>
+                </>,
+              ],
+              [
+                '',
+                <>
+                  {' '}
+                  <Color4>
+                    <span className="inline-block  scale-y-[1.5]">↓</span>
+                  </Color4>
+                </>,
+                <>
+                  <Color4>
+                    <span style={{ fontSize: 'small' }}>Zusammenfassen</span>
+                  </Color4>
+                </>,
+              ],
+              [
+                <>A</>,
+                <>=</>,
+                <>
+                  <Color2>{buildInlineFrac(<>x²</>, <>2</>)}</Color2> +{' '}
+                  <Color3>{data.länge} · h</Color3> +{' '}
+                  <Color2>{buildInlineFrac(<>x²</>, <>2</>)}</Color2>
+                </>,
+              ],
+              [
+                <>A</>,
+                <>=</>,
+                <>
+                  <Color2>x²</Color2> + <Color3>{data.länge} · h</Color3>
+                </>,
+              ],
+            ])}
+          </>
+        )
       },
     },
   ],
