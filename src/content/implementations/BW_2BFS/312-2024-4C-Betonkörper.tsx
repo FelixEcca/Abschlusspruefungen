48474427074c78a2966b39aa906be0b438380325
FelixEcca <163112991+FelixEcca@users.noreply.github.com>
2025-03-18 17:31:28 +0100

    4C fertig

--- src/content/implementations/BW_2BFS/312-2024-4C-Betonkörper.tsx
+++ src/content/implementations/BW_2BFS/312-2024-4C-Betonkörper.tsx
@@ -1,6 +1,16 @@
 import { Exercise } from '@/data/types'
+import { Color4 } from '@/helper/colors'
+import { buildEquation, buildInlineFrac } from '@/helper/math-builder'
+import { pp } from '@/helper/pretty-print'
+import { roundToDigits } from '@/helper/round-to-digits'
 
-interface DATA {}
+interface DATA {
+  länge: number
+  h1: number
+  h2: number
+  r: number
+  weight: number
+}
 
 export const exercise312: Exercise<DATA> = {
   title: 'Betonkörper',
@@ -8,22 +18,32 @@ export const exercise312: Exercise<DATA> = {
   useCalculator: true,
   duration: 42,
   generator(rng) {
-    return {}
+    return {
+      länge: rng.randomIntBetween(20, 50) / 10,
+      h1: rng.randomIntBetween(20, 50) / 10,
+      h2: rng.randomIntBetween(1, 7) / 10,
+      r: rng.randomIntBetween(20, 50) / 10,
+      weight: rng.randomIntBetween(15, 35) * 100,
+    }
   },
-  originalData: {},
+  originalData: { länge: 3.5, h1: 1.4, h2: 0.4, r: 0.7, weight: 2600 },
   constraint({ data }) {
-    return true
+    return 2 * data.r < data.länge
   },
   intro({ data }) {
     return (
       <>
         <p>
           Vor einem Museum steht der abgebildete Körper aus Beton. Er hat eine
-          quadratische Grundfläche mit der Seitenlänge 3,5 m.
+          quadratische Grundfläche mit der Seitenlänge {pp(data.länge)} m.
         </p>
+        <svg viewBox="0 0 328 80">
+          <image href="/content/BW_2BFS/312.png" height="80" width="328" />
+        </svg>
         <p>
-          Die weiteren Maße des Lörpers sind: <br></br>h<sub>1</sub> = 1,4
-          m;&nbsp;&nbsp; h<sub>2</sub> = 0,4 m;&nbsp;&nbsp; r = 0,7 m
+          Die weiteren Maße des Körpers sind: <br></br>h<sub>1</sub> ={' '}
+          {pp(data.h1)} m;&nbsp;&nbsp; h<sub>2</sub> = {pp(data.h2)}{' '}
+          m;&nbsp;&nbsp; r = {pp(data.r)} m
         </p>
       </>
     )
@@ -38,14 +58,107 @@ export const exercise312: Exercise<DATA> = {
         return (
           <>
             <p>
-              Berechnen Sie, wie schwer der Körper ist, wenn 1 m³ Beton 2600 kg
-              wiegt.
+              Berechnen Sie, wie schwer der Körper ist, wenn 1 m³ Beton{' '}
+              {data.weight} kg wiegt.
             </p>
           </>
         )
       },
       solution({ data }) {
-        return <></>
+        return (
+          <>
+            <p>
+              <b>Volumen des Körpers</b>
+            </p>
+            <p>Berechne das Volumen der kleinen Zylinder:</p>
+            {buildEquation([
+              [
+                <>
+                  V<sub>Zylinder</sub>
+                </>,
+                <>=</>,
+                <>
+                  π · r² · h<sub>2</sub>
+                </>,
+              ],
+              [
+                <></>,
+                <>=</>,
+                <>
+                  π · {pp(data.r)}² · {pp(data.h2)}
+                </>,
+              ],
+              [
+                <></>,
+                <>≈</>,
+                <>
+                  {pp(roundToDigits(Math.PI * data.r * data.r * data.h2, 2))}
+                </>,
+              ],
+            ])}
+            <p>Das Volumen aller vier Zylinder beträgt:</p>
+            <p>
+              4 · {pp(roundToDigits(Math.PI * data.r * data.r * data.h2, 2))} ={' '}
+              {pp(roundToDigits(4 * Math.PI * data.r * data.r * data.h2, 2))}
+            </p>
+
+            <p>Das Volumen des Quaders ist:</p>
+            {buildEquation([
+              [
+                <>
+                  V<sub>Quader</sub>
+                </>,
+                <>=</>,
+                <>
+                  l · b · h<sub>1</sub>
+                </>,
+              ],
+              [
+                <></>,
+                <>=</>,
+                <>
+                  {pp(data.länge)} · {pp(data.länge)} · {pp(data.h1)}
+                </>,
+              ],
+              [<></>, <>=</>, <>{pp(data.länge * data.länge * data.h1)}</>],
+            ])}
+            <p>Das gesamte Volumen ist damit:</p>
+            <p>
+              {pp(roundToDigits(4 * Math.PI * data.r * data.r * data.h2, 2))} +{' '}
+              {pp(data.länge * data.länge * data.h1)} ≈{' '}
+              {pp(
+                data.länge * data.länge * data.h1 +
+                  roundToDigits(4 * Math.PI * data.r * data.r * data.h2, 2),
+              )}
+            </p>
+            <p>
+              Das Volumen beträgt:{' '}
+              {pp(
+                data.länge * data.länge * data.h1 +
+                  roundToDigits(4 * Math.PI * data.r * data.r * data.h2, 2),
+              )}{' '}
+              m³
+            </p>
+            <p>
+              <b>Gewicht des Körpers</b>
+            </p>
+            <p>Der Körper wiegt damit insgesamt:</p>
+            <p>
+              {pp(
+                data.länge * data.länge * data.h1 +
+                  roundToDigits(4 * Math.PI * data.r * data.r * data.h2, 2),
+              )}{' '}
+              m³ · {data.weight}
+              {buildInlineFrac(<>kg</>, <>m³</>)} ={' '}
+              {pp(
+                data.weight *
+                  (data.länge * data.länge * data.h1 +
+                    roundToDigits(4 * Math.PI * data.r * data.r * data.h2, 2)),
+              )}{' '}
+              kg
+            </p>
+          </>
+        )
       },
     },
     {
@@ -65,7 +178,174 @@ export const exercise312: Exercise<DATA> = {
         )
       },
       solution({ data }) {
-        return <></>
+        return (
+          <>
+            <p>
+              <b>Oberfläche der Zylinder</b>
+            </p>
+            <p>
+              Die Oberfläche der Zylinder besteht jeweils aus der Mantelfläche
+              und einem Deckel:
+            </p>
+            {buildEquation([
+              [
+                <>
+                  O<sub>Zylinder</sub>
+                </>,
+                <>=</>,
+                <>
+                  M<sub>Zylinder</sub> + D<sub>Zylinder</sub>
+                </>,
+              ],
+              [
+                <></>,
+                <>=</>,
+                <>
+                  2 · π · r · h<sub>2</sub> + π · r²
+                </>,
+              ],
+              [
+                <></>,
+                <>=</>,
+                <>
+                  2 · π · {pp(data.r)} · {pp(data.h2)} + π · {pp(data.r)}²
+                </>,
+              ],
+              [
+                <></>,
+                <>≈</>,
+                <>
+                  {pp(
+                    roundToDigits(
+                      2 * Math.PI * data.r * data.h2 +
+                        Math.PI * data.r * data.r,
+                      2,
+                    ),
+                  )}
+                </>,
+              ],
+            ])}
+            <p>
+              <b>Restliche Fläche des Quaders</b>
+            </p>
+            <svg viewBox="0 0 328 80">
+              <image
+                href="/content/BW_2BFS/312_2.png"
+                height="80"
+                width="328"
+              />
+            </svg>
+            <p>
+              Die Oberfläche besteht aus den vier Seitenflächen und der
+              Deckelfläche um die Zylinder.
+            </p>
+            <div>
+              <span style={{ fontSize: '0.8em' }}>
+                {buildEquation([
+                  [
+                    <>
+                      O<sub>Quader</sub>
+                    </>,
+                    <>=</>,
+                    <>
+                      M<sub>Quader</sub> + D<sub>Quader</sub>
+                    </>,
+                  ],
+                  [
+                    '',
+                    <>
+                      {' '}
+                      <Color4>
+                        <span className="inline-block  scale-y-[1.5]">↓</span>
+                      </Color4>
+                    </>,
+                    <>
+                      <div>
+                        <span style={{ fontSize: '0.7em' }}>
+                          <Color4>
+                            Die Deckelfläche des Quaders ist l · l, wobei man 4
+                            Kreisflächen abziehen muss
+                          </Color4>
+                        </span>
+                      </div>
+                    </>,
+                  ],
+                  [
+                    <></>,
+                    <>=</>,
+                    <>
+                      4 · l · h<sub>1</sub> + l · l - 4 · π · r²
+                    </>,
+                  ],
+                  [
+                    <></>,
+                    <>=</>,
+                    <>
+                      4 · {pp(data.länge)} · {pp(data.h1)} + {pp(data.länge)} ·{' '}
+                      {pp(data.länge)} - 4 · π · {pp(data.r)}²
+                    </>,
+                  ],
+                  [
+                    <></>,
+                    <>≈</>,
+                    <>
+                      {pp(
+                        roundToDigits(
+                          4 * data.länge * data.h1 +
+                            data.länge * data.länge -
+                            4 * Math.PI * data.r * data.r,
+                          2,
+                        ),
+                      )}
+                    </>,
+                  ],
+                ])}
+              </span>
+            </div>
+
+            <p>
+              <b>Gesamtfläche</b>
+            </p>
+            <p>Die gesamte Fläche beträgt:</p>
+            <p>
+              <div>
+                <span style={{ fontSize: '0.8em' }}>
+                  O = O<sub>Quader</sub> + O<sub>Quader</sub> ={' '}
+                  {pp(
+                    roundToDigits(
+                      2 * Math.PI * data.r * data.h2 +
+                        Math.PI * data.r * data.r,
+                      2,
+                    ),
+                  )}{' '}
+                  +{' '}
+                  {pp(
+                    roundToDigits(
+                      4 * data.länge * data.h1 +
+                        data.länge * data.länge -
+                        4 * Math.PI * data.r * data.r,
+                      2,
+                    ),
+                  )}{' '}
+                  ={' '}
+                  {pp(
+                    roundToDigits(
+                      2 * Math.PI * data.r * data.h2 +
+                        Math.PI * data.r * data.r,
+                      2,
+                    ) +
+                      roundToDigits(
+                        4 * data.länge * data.h1 +
+                          data.länge * data.länge -
+                          4 * Math.PI * data.r * data.r,
+                        2,
+                      ),
+                  )}
+                </span>
+              </div>
+            </p>
+          </>
+        )
       },
     },
   ],
