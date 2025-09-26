fdd1d6035f66a5e49fc02e4d45edbd15b0a629d3
FelixEcca <163112991+FelixEcca@users.noreply.github.com>
2025-09-26 09:20:11 +0200

    Update 3111-2023-4A-Funktionen-und-Graphen.tsx

--- src/content/implementations/BW_2BFS/3111-2023-4A-Funktionen-und-Graphen.tsx
+++ src/content/implementations/BW_2BFS/3111-2023-4A-Funktionen-und-Graphen.tsx
@@ -35,7 +35,7 @@ export const exercise3111: Exercise<DATA3111> = {
     const a = rng.randomItemFromArray([1])
     const h = rng.randomItemFromArray([-3, -2, -1, 1, 2, 3, 4, 5, 6])
     const k = rng.randomItemFromArray([-3, -2, 1, 2, 3, 4, 5, 6, -4, -1])
-    const m = rng.randomItemFromArray([-3, -2, 1, 2, 3])
+    const m = rng.randomItemFromArray([-3, -2, -1, 2, 3])
     const d = rng.randomIntBetween(1, 3)
 
     return { a, h, k, m, d }
@@ -66,11 +66,11 @@ export const exercise3111: Exercise<DATA3111> = {
       <p>
         Gegeben ist die Gerade <InlineMath math="g" /> mit<br></br>{' '}
         <InlineMath
-          math={`y=${data.m == 1 ? '' : pp(data.m)}x${pp(data.d, 'merge_op')}`}
+          math={`y=${data.m == -1 ? '-' : pp(data.m)}x${pp(data.d, 'merge_op')}`}
         />{' '}
         und die Parabel <InlineMath math="p" /> mit<br></br>{' '}
         <InlineMath
-          math={`y=${data.a != 1 ? pp(data.a) : ''}x²${pp(-2 * data.a * data.h, 'merge_op')}x${pp(
+          math={`y=${data.a != 1 ? pp(data.a) : ''}x²${-2 * data.a * data.h != 0 ? pp(-2 * data.a * data.h, 'merge_op') + 'x' : ''}${pp(
             data.k + data.a * data.h * data.h,
             'merge_op',
           )}`}
@@ -98,23 +98,38 @@ export const exercise3111: Exercise<DATA3111> = {
       solution({ data }) {
         return (
           <>
-            <p>Bestimme die Scheitelpunktform mit quadratischer Ergänzung:</p>
-            <BlockMath
-              math={`y = x^{2}${pp(-2 * data.a * data.h, 'merge_op')}x + \\left(\\frac{${pp(Math.abs(-2 * data.a * data.h))}}{2}\\right)^2${pp(
-                data.k + data.a * data.h * data.h,
-                'merge_op',
-              )}- \\left(\\frac{${pp(Math.abs(-2 * data.a * data.h))}}{2}\\right)^2`}
-            />
-            <BlockMath
-              math={`y = \\left(x ${-2 * data.a * data.h > 0 ? '+' : '-'}\\frac{${pp(Math.abs(-2 * data.a * data.h))}}{2}\\right)^{2} ${pp(data.k + data.a * data.h * data.h - ((-2 * data.a * data.h) / 2) * ((-2 * data.a * data.h) / 2), 'merge_op')}`}
-            />
-            <BlockMath
-              math={`y = (x ${pp((-2 * data.a * data.h) / 2, 'merge_op')})^{2} ${pp(data.k + data.a * data.h * data.h - ((-2 * data.a * data.h) / 2) * ((-2 * data.a * data.h) / 2), 'merge_op')}`}
-            />
-            <BlockMath
-              math={`\\Rightarrow\\; S\\,=\\,(${pp(data.h)}\\mid ${pp(data.k)})`}
-            />
-            <BlockMath math={`S\\,=\\,(${pp(data.h)}\\mid ${pp(data.k)})`} />
+            {-2 * data.a * data.h != 0 ? (
+              <>
+                <p>
+                  Bestimme die Scheitelpunktform mit quadratischer Ergänzung:
+                </p>
+                <BlockMath
+                  math={`y = x^{2}${pp(-2 * data.a * data.h, 'merge_op')}x + \\left(\\frac{${pp(Math.abs(-2 * data.a * data.h))}}{2}\\right)^2${pp(
+                    data.k + data.a * data.h * data.h,
+                    'merge_op',
+                  )}- \\left(\\frac{${pp(Math.abs(-2 * data.a * data.h))}}{2}\\right)^2`}
+                />
+                <BlockMath
+                  math={`y = \\left(x ${-2 * data.a * data.h > 0 ? '+' : '-'}\\frac{${pp(Math.abs(-2 * data.a * data.h))}}{2}\\right)^{2} ${pp(data.k + data.a * data.h * data.h - ((-2 * data.a * data.h) / 2) * ((-2 * data.a * data.h) / 2), 'merge_op')}`}
+                />
+                <BlockMath
+                  math={`y = (x ${pp((-2 * data.a * data.h) / 2, 'merge_op')})^{2} ${pp(data.k + data.a * data.h * data.h - ((-2 * data.a * data.h) / 2) * ((-2 * data.a * data.h) / 2), 'merge_op')}`}
+                />
+                <BlockMath
+                  math={`\\Rightarrow\\; S\\,=\\,(${pp(data.h)}\\mid ${pp(data.k)})`}
+                />
+                <BlockMath
+                  math={`S\\,=\\,(${pp(data.h)}\\mid ${pp(data.k)})`}
+                />
+              </>
+            ) : (
+              <>
+                <p>
+                  Die Parabel p liegt mit dem Scheitel auf der y-Achse:<br></br>{' '}
+                  <BlockMath math={`S\\,=\\,(0\\mid 1)`} />
+                </p>
+              </>
+            )}
           </>
         )
       },
@@ -214,40 +229,64 @@ export const exercise3111: Exercise<DATA3111> = {
 
         return (
           <>
-            <p>Setze die Terme gleich:</p>
-            <div className="space-y-2">
-              <BlockMath
-                math={`${data.a != 1 ? pp(data.a) : ''}x²${pp(-2 * data.a * data.h, 'merge_op')}x${pp(
-                  data.k + data.a * data.h * data.h,
-                  'merge_op',
-                )}=${pp(data.m)}x${pp(data.d, 'merge_op')}`}
-              />
-              <BlockMath
-                math={`x^2${pp(B, 'merge_op')}x${pp(C, 'merge_op')}=0`}
-              />
-              <BlockMath
-                math={`x_{1,2}=-\\frac{${pp(B)}}{2}\\pm\\sqrt{\\left(\\frac{${pp(B)}}{2}\\right)^2-${pp(C)}}`}
-              />
-              <BlockMath
-                math={`x_{1,2}=-\\frac{${pp(B)}}{2}\\pm\\sqrt{${pp((B * B) / 4 - C)}}`}
-              />
-              <BlockMath math={`x_1=${pp(x1)},\\quad x_2=${pp(x2)}`} />
-              <p>Berechne die y-Werte mithilfe der Geradengleichung:</p>
-              <BlockMath
-                math={`y_1=${pp(data.m)}\\cdot ${pp(x1)}${pp(
-                  data.d,
-                  'merge_op',
-                )}=${pp(y1)},\\quad y_2=${pp(data.m)}\\cdot ${pp(x2)}${pp(
-                  data.d,
-                  'merge_op',
-                )}=${pp(y2)}`}
-              />
-              <BlockMath
-                math={`\\Rightarrow\\; S_1(${pp(x1)}\\mid ${pp(y1)}),\\; S_2(${pp(
-                  x2,
-                )}\\mid ${pp(y2)})`}
-              />
-            </div>
+            {data.h === 0 && (
+              <>
+                <p>Setze die Terme gleich:</p>
+                <BlockMath math={`2x²-1=-x+2`} />
+                <BlockMath math={`2x²+x-3=0`} />
+                <BlockMath math={`x²+0,5x-1,5=0`} />
+                <p>Löse mithilfe der pq-Formel: </p>
+                <BlockMath
+                  math={`x_{1,2}=-\\frac{0,5}{2}\\pm\\sqrt{\\left(\\frac{0,5}{2}\\right)^2+1,5}`}
+                />
+                <BlockMath math={`x_{1,2}=-0,25\\pm\\sqrt{\\frac{25}{16}}`} />
+                <BlockMath math={`x_{1,2}=-0,25\\pm\\frac{5}{4}`} />
+                <BlockMath math={`x_1=-1,5,\\quad x_2=1`} />
+                <p>Berechne die y-Werte mithilfe der Geradengleichung:</p>
+                <BlockMath math={`y_1=1,5+2=3,5,\\quad y_2=-1+2=1`} />
+                <BlockMath
+                  math={`\\Rightarrow\\; S_1(-1,5\\mid 3,5),\\; S_2(1\\mid 1)`}
+                />
+              </>
+            )}
+            {data.h != 0 && (
+              <>
+                <p>Setze die Terme gleich:</p>
+                <div className="space-y-2">
+                  <BlockMath
+                    math={`${data.a != 1 ? pp(data.a) : ''}x²${pp(-2 * data.a * data.h, 'merge_op')}x${pp(
+                      data.k + data.a * data.h * data.h,
+                      'merge_op',
+                    )}=${pp(data.m)}x${pp(data.d, 'merge_op')}`}
+                  />
+                  <BlockMath
+                    math={`x^2${pp(B, 'merge_op')}x${pp(C, 'merge_op')}=0`}
+                  />
+                  <BlockMath
+                    math={`x_{1,2}=-\\frac{${pp(B)}}{2}\\pm\\sqrt{\\left(\\frac{${pp(B)}}{2}\\right)^2-${pp(C)}}`}
+                  />
+                  <BlockMath
+                    math={`x_{1,2}=-\\frac{${pp(B)}}{2}\\pm\\sqrt{${pp((B * B) / 4 - C)}}`}
+                  />
+                  <BlockMath math={`x_1=${pp(x1)},\\quad x_2=${pp(x2)}`} />
+                  <p>Berechne die y-Werte mithilfe der Geradengleichung:</p>
+                  <BlockMath
+                    math={`y_1=${pp(data.m)}\\cdot ${pp(x1)}${pp(
+                      data.d,
+                      'merge_op',
+                    )}=${pp(y1)},\\quad y_2=${pp(data.m)}\\cdot ${pp(x2)}${pp(
+                      data.d,
+                      'merge_op',
+                    )}=${pp(y2)}`}
+                  />
+                  <BlockMath
+                    math={`\\Rightarrow\\; S_1(${pp(x1)}\\mid ${pp(y1)}),\\; S_2(${pp(
+                      x2,
+                    )}\\mid ${pp(y2)})`}
+                  />
+                </div>
+              </>
+            )}
           </>
         )
       },
