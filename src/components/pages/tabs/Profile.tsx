4e534c162c518a1178a86f0a9667d6886b918e34
FelixEcca <163112991+FelixEcca@users.noreply.github.com>
2025-10-09 08:38:14 +0200

    Update Profile.tsx

--- src/components/pages/tabs/Profile.tsx
+++ src/components/pages/tabs/Profile.tsx
@@ -81,6 +81,7 @@ export function Profile() {
                     updatePlayerProfileStore(s => {
                       s.currentExam = v
                     })
+                    window.location.reload()
                   }
                 }}
                 className="p-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
