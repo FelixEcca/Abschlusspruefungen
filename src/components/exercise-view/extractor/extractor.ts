74f7132270f809c8530d5aae810fad83b18a5794
FelixEcca <163112991+FelixEcca@users.noreply.github.com>
2025-12-04 09:28:51 +0100

    Update extractor.ts

--- src/components/exercise-view/extractor/extractor.ts
+++ src/components/exercise-view/extractor/extractor.ts
@@ -4,38 +4,63 @@ import { renderToStaticMarkup } from 'react-dom/server'
 import { ExtractorStore } from './extractor-store'
 import { countLetter } from '@/helper/count-letter'
 
+export type ExtractorOptions = {
+  includeSolution?: boolean
+  includeCorrectionHints?: boolean
+  includeIntroLabel?: boolean
+}
+
 // returning html string
-export function extractor(exercise: Exercise<any>, data: object) {
+export function extractor(
+  exercise: Exercise<any>,
+  data: object,
+  options?: ExtractorOptions,
+) {
+  const {
+    includeSolution = true,
+    includeCorrectionHints = true,
+    includeIntroLabel = true,
+  } = options ?? {}
+
   ExtractorStore.active = true
   let output = ''
+
   if ('tasks' in exercise) {
-    output += 'Hier beginnt das Intro der Aufgabe:\n\n'
+    if (includeIntroLabel) {
+      output += 'Hier beginnt das Intro der Aufgabe:\n\n'
+    }
     output += renderToStaticMarkup(exercise.intro({ data }))
+
     exercise.tasks.forEach((t, i) => {
       output += '\n\nTeilaufgabe ' + countLetter('a', i) + ')\n\n'
-      output +=
-        renderToStaticMarkup(t.task({ data })) +
-        '\n\nLösung:\n\n' +
-        renderToStaticMarkup(t.solution({ data }))
-      if (t.correctionHints) {
+      output += renderToStaticMarkup(t.task({ data }))
+
+      if (includeSolution) {
+        output +=
+          '\n\nLösung:\n\n' + renderToStaticMarkup(t.solution({ data }))
+      }
+
+      if (includeCorrectionHints && t.correctionHints) {
         output +=
           '\n\nKorrekturhinweise:\n\n' +
           renderToStaticMarkup(t.correctionHints({ data }))
       }
     })
-    for (const t of exercise.tasks) {
-    }
   } else {
-    output =
-      renderToStaticMarkup(exercise.task({ data })) +
-      '\n\nLösung:\n\n' +
-      renderToStaticMarkup(exercise.solution({ data }))
-    if (exercise.correctionHints) {
+    output = renderToStaticMarkup(exercise.task({ data }))
+
+    if (includeSolution) {
+      output +=
+        '\n\nLösung:\n\n' + renderToStaticMarkup(exercise.solution({ data }))
+    }
+
+    if (includeCorrectionHints && exercise.correctionHints) {
       output +=
         '\n\nKorrekturhinweise:\n\n' +
         renderToStaticMarkup(exercise.correctionHints({ data }))
     }
   }
+
   console.log(output)
   ExtractorStore.active = false
   return output
