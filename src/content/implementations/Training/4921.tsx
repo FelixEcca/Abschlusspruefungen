6dcb15e99897fc03ceca4dec0f65d9fddeb5b24a
FelixEcca <163112991+FelixEcca@users.noreply.github.com>
2026-04-21 17:30:06 +0200

    Update 4921.tsx

--- src/content/implementations/Training/4921.tsx
+++ src/content/implementations/Training/4921.tsx
@@ -50,14 +50,16 @@ export const exercise4921: Exercise<DATA> = {
   constraint({ data }) {
     const xs = -data.b / (2 * data.a)
     const ys = data.a * xs * xs + data.b * xs + data.c
-    return data.a !== 0 && data.b!=0 && data.c!=0 && ys%1===0
+    return data.a !== 0 && data.b != 0 && data.c != 0 && ys % 1 === 0
   },
 
   task({ data }) {
     return (
       <>
-        <p>Berechnen Sie den Scheitelpunkt <InlineMath
-          math={`S(x|y)`} /> der Parabel.</p>
+        <p>
+          Berechnen Sie den Scheitelpunkt <InlineMath math={`S(x|y)`} /> der
+          Parabel.
+        </p>
         <InlineMath
           math={`y=${pp(data.a)}x^2${pp(data.b, 'merge_op')}x${pp(
             data.c,
@@ -76,13 +78,14 @@ export const exercise4921: Exercise<DATA> = {
       <>
         <p>Mit der Formel für die x-Koordinate des Scheitels:</p>
         <InlineMath
-          math={`x_S=\\frac{-b}{2a}=\\frac{-(${pp(data.b)})}{2\\cdot${pp(
+          math={`x_S=\\frac{-b}{2a}=\\frac{${pp(-data.b)}}{2\\cdot${pp(
             data.a,
+            'embrace_neg',
           )}}=${ppFrac(xs)}`}
         />
-        <p>Einsetzen in die Funktion:</p>
+        <p>Einsetzen in die Gleichung:</p>
         <InlineMath
-          math={`y_S=${pp(data.a)}\\cdot(${ppFrac(xs)})^2${pp(
+          math={`y_S=${pp(data.a)}\\cdot${pp(xs, 'embrace_neg')}^2${pp(
             data.b * xs,
             'merge_op',
           )}${pp(data.c, 'merge_op')}=${ppFrac(ys)}`}
